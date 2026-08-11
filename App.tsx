5986dd8249a3af234bf17f05ed734052d5cb1de2
Katleho <katliradebe19@gmail.com>
2026-08-11 14:51:41 +0200

    code:data passed to screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import{useState}from 'react';
-import { Button,StyleSheet, Text,TextInput,View } from 'react-native';
-
+import { Button,StyleSheet, Text,TextInput,TouchableHighlight,View } from 'react-native';
+Highlight
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack=createNativeStackNavigator();
@@ -19,10 +19,113 @@ export default function App() {
 function PlayerScreen({navigation}:any){
  const[player1,setPlayer1]=useState<string>('');
   const[player2,setPlayer2]=useState<string>('');
+  const[player1Turn,setPlayer1Turn]=useState<boolean>(true);
 
+  const handlePress=(index:number)=>{
+    setPlayer1Turn(!player1Turn);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Tic-Tac-Toe!</Text>
+
+    <Text>
+      {player1Turn? $
+     </Text>
+
+    <View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(0)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(1)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(2)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(3)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(4)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(5)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(6)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(7)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View><View style={styles.row}>
+    <TouchableHighlight style={styles.cell} onPress={()=> handlePress(8)}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+    <TouchableHighlight style={styles.cell}>
+        <Text style={styles.cellText}></Text>
+      </TouchableHighlight>
+  </View>
+    
+
+
 
     <TextInput
       placeholder='Player 1 Name'
@@ -48,6 +151,7 @@ function GameScreen({navigation,route}:any){
   return(
     <view style={styles.container}>
       <Text style={styles.title}>{player1} vs {player2}</Text>
+
     </view>
   );
 }
@@ -75,4 +179,20 @@ marginVertical:12,
     width:'80%',
   
   },
+  row: {
+    flexDirection:'row',
+  },
+  cell:{
+    width:80,
+    height:80,
+    borderWidth:1,
+    borderColor:'#333',
+    justifyContent:'center',
+    alignItems:'center',
+    margin:2,
+  },
+  cellText:{
+      fontSize:40,
+      fontWeight:'bold',
+  }
 });
